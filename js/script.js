409c2ff5c45ef63119fd91341d8ae3490e02313f
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-10-19 16:14:40 +0200

    PSW GENERATOR | > start

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+var  nome = prompt("Qual'è il tuo nome? (digita il nome senza spazi)") ;
+alert ( "Ciao " + nome);
+
+var  cognome = prompt("Qual'è il tuo cognome?")
+alert ( "Piacere " + nome + " " + cognome);
+
+var  colore = prompt( nome +", qual'è il tuo colore preferito?")
+
+document.getElementById("password").innerHTML = nome + cognome + colore + "19"
